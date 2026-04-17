f903ad81b8e4342860fd4be37550f6e9f0c8d4fd
glam cars <ouaroudabderrahman2020@gmail.com>
2026-04-17 11:36:32 +0100

    fix: merged vite config and added base path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/simple-car-rental-2/', // This allows GitHub Pages to find your files
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss(), // Keep all your plugins in this one list
   ],
 })
