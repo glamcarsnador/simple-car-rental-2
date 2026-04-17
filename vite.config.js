e7f7df78b8d4653accc6fa8643c010929cd9b8fb
glam cars <ouaroudabderrahman2020@gmail.com>
2026-04-17 11:55:39 +0100

    site ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/simple-car-rental-2/', // This allows GitHub Pages to find your files
+  base: '/simple-car-rental-2/',
   plugins: [
     react(),
-    tailwindcss(), // Keep all your plugins in this one list
+    tailwindcss(),
   ],
 })
